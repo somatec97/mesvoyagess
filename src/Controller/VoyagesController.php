8e5edfa68ad36617a0eabc8b9523eafef2128e5f
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-12-09 08:14:40 +0100

    Phase 4: plusieurs pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of VoyagesController
+ *
+ * @author BEN BAHA
+ */
+class VoyagesController extends AbstractController {
+    /**
+     * @Route("/voyages", name="voyages")
+     * @return Response
+     */
+    public function index(): Response {
+        return$this->render("pages/voyages.html.twig");
+        
+    }
+    
+}
