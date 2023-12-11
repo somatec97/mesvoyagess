535cda3c6c4cb059c6231b00d89d7aca0e886974
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-12-11 17:12:40 +0100

    Phase 10:ORM(filtrage des enregistrements)

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -32,6 +32,31 @@ class VisiteRepository extends ServiceEntityRepository
                 ->getQuery()
                 ->getResult();
     }
+    /**
+     *§Enregistrements dont un champ est egale a une valeur
+     * ou tous les enregistrements si la valeur est vide! 
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
+    public function findByEqualValue($champ, $valeur):array {
+        if($valeur==""){
+            return $this->createQueryBuilder('v')// alias de la table
+                    ->orderBy('v.'.$champ, 'ASC')
+                    ->getQuery()
+                    ->getResult();
+            
+        }else{
+            return $this->createQueryBuilder('v')//alias de la table
+                    ->where('v.'.$champ.'=:valeur')
+                    ->setParameter('valeur', $valeur)
+                    ->orderBy('v.datecreation', 'DESC')
+                    ->getQuery()
+                    ->getResult();
+                    
+        }
+        
+    }
 //    /**
 //     * @return Visite[] Returns an array of Visite objects
 //     */
