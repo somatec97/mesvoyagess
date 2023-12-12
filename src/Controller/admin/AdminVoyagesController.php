a9b1837a4a782f4961ec9d08646f9862a00a8a20
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-12-12 13:07:31 +0100

    Phase 12:ORM(suppression d'un enregistrement)

---
+++ src/Controller/admin/AdminVoyagesController.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller\admin;
+
+use App\Entity\Visite;
+use App\Repository\VisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminVoyagesController
+ *
+ * @author BEN BAHA
+ */
+class AdminVoyagesController extends AbstractController{
+    /**
+     * @Route("/admin", name="admin.voyages")
+     * @return Response
+     */
+    public function index(): Response {
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return$this->render("admin/admin.voyages.html.twig", [
+            'visites' => $visites
+        ]);
+        
+    }
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    /**
+     * @Route("/admin/suppr/{id}", name="admin.voyage.suppr")
+     * @param Visite $visite
+     * @return Response
+     */
+    public function suppr(Visite $visite):Response {
+        $this->repository->remove($visite, true);
+        return $this->redirectToRoute('admin.voyages');
+        
+    }
+}
+
+
