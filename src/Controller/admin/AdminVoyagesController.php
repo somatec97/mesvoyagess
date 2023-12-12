f1a13b4b329c263894fbce2ca953cdf0eb9e2a20
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-12-12 16:51:00 +0100

    Phase 13: ORM(modification d'un enregistrement, création d'un formulaire)

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -8,8 +8,10 @@
 namespace App\Controller\admin;
 
 use App\Entity\Visite;
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,7 +27,7 @@ class AdminVoyagesController extends AbstractController{
      */
     public function index(): Response {
         $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
-        return$this->render("admin/admin.voyages.html.twig", [
+        return $this->render("admin/admin.voyages.html.twig", [
             'visites' => $visites
         ]);
         
@@ -50,6 +52,25 @@ class AdminVoyagesController extends AbstractController{
     public function suppr(Visite $visite):Response {
         $this->repository->remove($visite, true);
         return $this->redirectToRoute('admin.voyages');
+        
+    }
+    /**
+     * @Route("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param Visite $visite
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Visite $visite, Request $request):Response {
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->repository->add($visite, true);
+            return $this->redirectToRoute('admin.voyages');
+        }
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);
         
     }
 }
