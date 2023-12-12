7fd1d8b36b49516dff0df0c15f467577a6628d06
BEN BAHA <soumia.azza.91.97@gmail.com>
2023-12-12 17:26:52 +0100

    Phase 14: ORM(ajout d'un enregistrement)

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class VisiteType extends AbstractType
 {
@@ -15,8 +16,10 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation', null, [
+            ->add('datecreation', DateType::class, [
                 'widget' => 'single_text',
+                'data' => isset($options['data']) &&
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() :new DateTime('now'),
                 'label' => 'date'
             ])
             ->add('note')
